3ca43aeb8eed52c837f0b1836300a5b5019a2590
G-san <developer@woofer-empowerment.com>
2025-02-19 18:21:15 +0900

    ローカルDBのサンプル追加処理

--- lib/pages/home/home_view_model.dart
+++ lib/pages/home/home_view_model.dart
@@ -17,6 +17,13 @@ class HomeViewModel extends AsyncNotifier<List<Product>> {
   }
 
   Future<List<Product>> _loadItems() async {
+    await Future.delayed(const Duration(milliseconds: 2000)); //サンプルとして読み込みを２秒間待つようにする
+    final products = await productRepository.getAllProducts();
+    if (products.length != 0) return products;
+
+    //サンプル処理
+    print("サンプル商品の追加");
+    await productRepository.insertSampleProducts();
     return await productRepository.getAllProducts();
   }
 }
